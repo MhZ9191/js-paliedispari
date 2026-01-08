560518d2ec8cd5e847936ffc2c7f75fdba3ac20d
MhZ9191 <hegic91@gmail.com>
2026-01-08 17:41:35 +0100

    Added prompt for user input

--- pari_e_dispari/script.js
+++ pari_e_dispari/script.js
@@ -6,3 +6,17 @@ Sommiamo i due numeri
 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.
 */
+
+let userChoice = prompt("Scegli tra pari o dispari!")
+  .trim()
+  .toLocaleLowerCase();
+//ciclo finchè non inserisco pari o dispari
+while (userChoice != "pari" && userChoice != "dispari") {
+  userChoice = prompt("Scegli tra pari o dispari!").trim().toLocaleLowerCase();
+}
+
+let userNumber = parseInt(prompt("Inserisci un numero da 1 a 5"));
+//ciclo per un numero compreso da 1 a 5
+while (userNumber < 1 || userNumber > 5 || isNaN(userNumber)) {
+  userNumber = parseInt(prompt("Inserisci un numero da 1 a 5"));
+}
