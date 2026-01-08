6e657e385a1f98182256cd2a62b37eeacce3f083
MhZ9191 <hegic91@gmail.com>
2026-01-08 16:35:45 +0100

    Added another approach "palindromo"

--- palindromo/script.js
+++ palindromo/script.js
@@ -8,23 +8,31 @@ function verifyPal(uWord) {
   //Primo approccio
   //   let tmpW = uWord.split("").reverse().join("");
   //   return tmpW === uWord;
-
+  //
   //secondo approccio
   //   const tmpW = [];
   //   for (let i = 0; i < uWord.length; i++) {
   //     tmpW[i] = uWord[uWord.length - (i + 1)];
   //   }
-  //   return uWord == tmpW.join("");
-
+  //   return uWord === tmpW.join("");
+  //
   //un altro approccio
+  //   const lenW = uWord.trim().length / 2;
+  //   for (let i = 0; i < lenW; i++) {
+  //     if (uWord[i] != uWord[uWord.length - (i + 1)]) {
+  //       return false;
+  //     }
+  //   }
+  //   return true;
+  //
+  //un altro modo?
 
-  const lenW = uWord.trim().length / 2;
-  for (let i = 0; i < lenW; i++) {
-    if (uWord[i] != uWord[uWord.length - (i + 1)]) {
-      return false;
-    }
+  const tmpW = [];
+  for (let i = 0; i < uWord.length; i++) {
+    tmpW.unshift(uWord[i]);
   }
-  return true;
+
+  return tmpW.join("") === uWord;
 }
 
 const userWord = "AnnA" || prompt("Inserisci la parola da verificare!").trim();
