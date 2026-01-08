65fb62d63887d75a2be114b62aaedd3b179fb8b4
MhZ9191 <hegic91@gmail.com>
2026-01-08 15:51:12 +0100

    Added another approach "palindromo"

--- palindromo/script.js
+++ palindromo/script.js
@@ -6,9 +6,19 @@ Creare una funzione per capire se la parola inserita è palindroma
 
 function verifyPal(uWord) {
   //Primo approccio
-  let tmpW = uWord.split("").reverse().join("");
-  return tmpW === uWord;
+  //   let tmpW = uWord.split("").reverse().join("");
+  //   return tmpW === uWord;
+
+  //secondo approccio
+
+  let tmpW = [];
+  for (let i = 0; i < uWord.length; i++) {
+    tmpW[i] = uWord[uWord.length - (i + 1)];
+  }
+
+  return uWord == tmpW.join("");
 }
+
 const userWord = "AnnA" || prompt("Inserisci la parola da verificare!").trim();
 const verifyUserWord = verifyPal(userWord);
 
